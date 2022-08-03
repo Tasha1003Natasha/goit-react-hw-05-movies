479c308042bd90c9bb71d89c9a4e2be13c3f6a53
Tasha1003Natasha <102981670+Tasha1003Natasha@users.noreply.github.com>
2022-08-03 10:56:24 +0000

    Deploying to gh-pages from @ Tasha1003Natasha/goit-react-hw-05-movies@3454936f86b269c9b93fae8aece9058afb1e8ade 🚀

---
+++ static/js/358.8f13dfc9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[358],{3358:function(e,t,n){n.r(t);var r=n(3504),o=n(184);t.default=function(){return(0,o.jsx)("div",{children:(0,o.jsxs)("p",{children:["Page now found go to the ",(0,o.jsx)(r.rU,{to:"/",children:"Open home page"})]})})}}}]);
+//# sourceMappingURL=358.8f13dfc9.chunk.js.map
